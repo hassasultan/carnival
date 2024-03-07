bf11e6dd8bbcb01cb4b110d7bc9b7d3a36de45c3
hassasultan <h.hsultan277@gmail.com>
2024-03-07 18:41:17 +0500

    updted ui

--- resources/views/dashboard/admin/layouts/app.blade.php
+++ resources/views/dashboard/admin/layouts/app.blade.php
@@ -180,6 +180,239 @@
     <script src="{{ asset('assets/js/ifbreakpoint.js') }}"></script>
     <script src="{{ asset('assets/js/config.js') }}"></script>
     <script src="{{ asset('assets/js/theme.switcher.js') }}"></script>
+    <script src="{{ asset('assets/js/d3.min.js') }}"></script>
+  <script src="{{ asset('assets/js/leaflet.js') }}"></script>
+
+  <script src="{{ asset('assets/js/map.world.js') }}"></script>
+  <script src="{{ asset('assets/js/map.euro.js') }}"></script>
+  <script src="{{ asset('assets/js/map.us.js') }}"></script>
+  <script src="{{ asset('assets/js/maps.custom.js') }}"></script>
+
+  <script src="{{ asset('assets/js/Chart.min.js') }}"></script>
+  <script>
+    /* defind global options */
+    Chart.defaults.global.defaultFontFamily = cfFontFamily;
+    Chart.defaults.global.defaultFontColor = colors.mutedColor;
+  </script>
+  <script src="{{ asset('assets/js/gauge.min.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.sparkline.min.js') }}"></script>
+
+  <script src="{{ asset('assets/js/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('assets/js/apexcharts.custom.js') }}"></script>
+  <script src='{{ asset('assets/js/jquery.dataTables.min.js') }}'></script>
+  <script src='{{ asset('assets/js/dataTables.bootstrap4.min.js') }}'></script>
+  <script>
+    $('#dataTable-1').DataTable({
+      autoWidth: true,
+      "lengthMenu": [
+        [16, 32, 64, -1],
+        [16, 32, 64, "All"]
+      ]
+    });
+    $('#dataTable-2').DataTable({
+      autoWidth: true,
+      lengthMenu: [
+        [16, 32, 64, -1],
+        [16, 32, 64, "All"]
+      ],
+      language: { search: '', searchPlaceholder: "Search..." },
+      order: [],
+      columnDefs: [
+        { targets: 'no-sort', orderable: false }
+      ],
+      dom:  "<'row align-items-center'<'col-sm-12 col-md-6 text-muted mb-2'i>"+"<'col-sm-12 col-md-6 text-muted mb-2'f>>" +
+            "<'row mb-4'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-12 col-md-5 text-muted'l><'col-sm-12 col-md-7'p>>",
+    });
+    
+    //custom search input
+    $('.dataTables_filter input').removeClass('form-control-sm');
+    $('.dataTables_filter input').addClass('form-control');
+
+    //custom page #
+    $('.dataTables_length select').removeClass('form-control-sm');
+    $('.dataTables_length select').addClass('form-control');
+    
+  </script>
+  <script src='{{ asset('assets/js/jquery.mask.min.js') }}'></script>
+  <script src='{{ asset('assets/js/select2.min.js') }}'></script>
+  <script src='{{ asset('assets/js/jquery.steps.min.js') }}'></script>
+  <script src='{{ asset('assets/js/jquery.validate.min.js') }}'></script>
+  <script src='{{ asset('assets/js/jquery.timepicker.js') }}'></script>
+  <script src='{{ asset('assets/js/dropzone.min.js') }}'></script>
+  <script src='{{ asset('assets/js/uppy.min.js') }}'></script>
+  <script src='{{ asset('assets/js/ion.rangeSlider.min.js') }}'></script>
+  <script src='{{ asset('assets/js/jQuery.tagify.min.js') }}'></script>
+  
+  <script src='{{ asset('assets/js/quill.min.js') }}'></script>
+  <script>
+    $('.select2').select2({
+      theme: 'bootstrap4',
+    });
+    $('.select2-multi').select2({
+      multiple: true,
+      theme: 'bootstrap4',
+    });
+
+    $('.drgpicker').daterangepicker({
+      singleDatePicker: true,
+      timePicker: false,
+      showDropdowns: true,
+      locale: {
+        format: 'MM/DD/YYYY'
+      }
+    });
+
+    $('.time-input').timepicker({
+      'scrollDefault': 'now',
+      'zindex' : '9999' /* fix modal open */
+    });
+
+    /** date range picker */
+    if($('.datetimes').length) {
+      $('.datetimes').daterangepicker({
+        timePicker: true,
+        startDate: moment().startOf('hour'),
+        endDate: moment().startOf('hour').add(32, 'hour'),
+        locale: {
+          format: 'M/DD hh:mm A'
+        }
+      });
+    } 
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+      $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+
+    $('#reportrange').daterangepicker({
+      startDate: start,
+      endDate: end,
+      ranges: {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      }
+    }, cb);
+
+    cb(start, end);
+
+    $('.input-placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+    $('.input-zip').mask('00000-000', {placeholder: "____-___"});
+    $('.input-money').mask("#.##0,00", {reverse: true});
+    $('.input-phoneus').mask('(000) 000-0000');
+    $('.input-mixed').mask('AAA 000-S0S');
+    $('.input-ip').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+      translation: {
+        'Z': {
+          pattern: /[0-9]/, optional: true
+        }
+      },
+      placeholder: "___.___.___.___"
+    });
+
+    // editor
+    var editor = document.getElementById('editor');
+
+    if(editor) {
+      var toolbarOptions = [
+        [{ 'font': [] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+
+        [{ 'header': 1 }, { 'header': 2 }],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+      ];
+
+      var quill = new Quill(editor, {
+        modules: {
+          toolbar: toolbarOptions
+        },
+        theme: 'snow'
+      });
+    }
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
+
+    //ion-rangeslider
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 500,
+        grid: true
+    });
+
+    var tinput = $('input[name=tags-jquery]').tagify({
+            whitelist : [
+                {"id":1, "value":"some string"}
+            ]
+        })
+        .on('add', function(e, tagName){
+            console.log('JQEURY EVENT: ', 'added', tagName)
+        })
+        .on("invalid", function(e, tagName) {
+            console.log('JQEURY EVENT: ',"invalid", e, ' ', tagName);
+        });
+
+    // get the Tagify instance assigned for this jQuery input object so its methods could be accessed
+    var jqTagify = tinput.data('tagify');
+
+  </script>
+  <script>
+    var uptarg = document.getElementById('drag-drop-area');
+    if(uptarg) {
+      var uppy = Uppy.Core()
+      .use(Uppy.Dashboard, {
+        inline: true,
+        target: uptarg,
+        proudlyDisplayPoweredByUppy: false,
+        theme: 'dark',
+        width: 770,
+        height: 210,
+        plugins: ['Webcam']
+      })
+      .use(Uppy.Tus, {
+        endpoint: 'https://master.tus.io/files/'
+      });
+
+      uppy.on('complete', (result) => {
+        console.log('Upload complete! We’ve uploaded these files:', result.successful)
+      });
+    }
+  </script>
     <script src="{{ asset('assets/js/apps.js') }}"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56159088-1"></script>
