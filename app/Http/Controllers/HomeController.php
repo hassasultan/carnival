6f05b9d8c6b01f4dfbc538f6dd2d504ef5192c2e
hassasultan <h.hsultan277@gmail.com>
2024-03-11 15:30:48 +0500

    pushed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(Auth::user());
+        dd(Auth::user()->toArray());
         if (Auth::user()->isAdmin()) 
         {
             return redirect()->route('admin');
