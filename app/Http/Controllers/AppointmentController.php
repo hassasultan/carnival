5aae4655537b37a6aabbb8b0e1b6e2a0c6f8870c
hassasultan <h.hsultan277@gmail.com>
2024-04-14 16:45:55 +0500

    pushed

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Appointment;
+
+class AppointmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('appointments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'user_id' => 'required',
+            'service_id' => 'required',
+            'appointment_datetime' => [
+                'required',
+                'date',
+                // Validate that the appointment date does not conflict with existing appointments
+                Rule::unique('appointments')->where(function ($query) use ($request) {
+                    return $query->where([
+                        ['user_id', $request->user_id],
+                        ['service_id', $request->service_id],
+                        ['appointment_datetime', $request->appointment_datetime],
+                    ]);
+                }),
+            ],
+        ]);
+    
+        Appointment::create($request->all());
+    
+        return redirect()->route('appointments.index')
+            ->with('success', 'Appointment booked successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
