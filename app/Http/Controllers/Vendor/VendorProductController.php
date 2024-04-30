83ef0618fb2a4181e7669a602117680e1a1b463e
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-04-30 13:46:11 +0500

    d

--- app/Http/Controllers/Vendor/VendorProductController.php
+++ app/Http/Controllers/Vendor/VendorProductController.php
@@ -38,6 +38,42 @@ class VendorProductController extends Controller
         return view('dashboard.vendor.products.create', compact('categories', 'variants'));
     }
 
+    // public function store(Request $request)
+    // {
+    //     // dd($request->all());
+    //     $request->validate([
+    //         'title' => 'required',
+    //         'category_id' => 'required',
+    //         'description' => 'required',
+    //         'old_price' => 'required',
+    //         'new_price' => 'required',
+    //         'status' => 'required',
+    //         'variant_id' => 'required|array',
+    //         'variant_id.*' => 'exists:variants,id',
+    //     ]);
+
+    //     $data = $request->all();
+
+    //     // Check if variant_images are present in the request
+    //     if ($request->hasFile('variant_images')) {
+    //         $data['variant_images'] = $request->file('variant_images');
+    //     } else {
+    //         // If no variant images are uploaded, set an empty array
+    //         $data['variant_images'] = [];
+    //     }
+
+    //     $product = $this->productService->createProduct($data);
+
+    //     if ($product) {
+    //         $products = Product::where('user_id', Auth::id())->get();
+    //         $view = view('dashboard.vendor.products.table', compact('products'))->render();
+
+    //         return response()->json(['message' => 'Product created successfully', 'table_html' => $view], 200);
+    //     } else {
+    //         return response()->json(['error' => 'Failed to create Product'], 500);
+    //     }
+    // }
+
     public function store(Request $request)
     {
         // dd($request->all());
@@ -54,13 +90,9 @@ class VendorProductController extends Controller
 
         $data = $request->all();
 
-        // Check if variant_images are present in the request
-        if ($request->hasFile('variant_images')) {
-            $data['variant_images'] = $request->file('variant_images');
-        } else {
-            // If no variant images are uploaded, set an empty array
-            $data['variant_images'] = [];
-        }
+        $image = $this->uploadImage($request->file('image'), 'productImage');
+
+        $data['image'] = $image;
 
         $product = $this->productService->createProduct($data);
 
