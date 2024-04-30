a3c48a4a8aa1f11faaacd0c070fa44e1b55730c8
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-04-30 10:43:17 +0500

    s

--- resources/views/ShopFrontend/Layouts/header.blade.php
+++ resources/views/ShopFrontend/Layouts/header.blade.php
@@ -198,141 +198,80 @@
 
                         <div class="nav-right">
 
-                            <!-- block mini cart -->
-                            <div class="block-minicart dropdown">
-                                <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
-                                    <span class="cart-icon"></span>
-                                    <span class="counter qty">
-                                        <span class="cart-text">Shopping Cart</span>
-                                        <span class="counter-number">{{ $cartItems->count() }}</span>
-                                        <span class="counter-label">{{ $cartItems->count() }} <span>Items</span></span>
-                                        <span class="counter-price">${{ $total }}</span>
-                                    </span>
-                                </a>
-                                {{-- <div class="dropdown-menu">
-                                    <form>
-                                        <div  class="minicart-content-wrapper" >
-                                            <div class="subtitle">
-                                                You have 6 item(s) in your cart
-                                            </div>
-                                            <div class="minicart-items-wrapper">
-                                                <ol class="minicart-items">
-                                                    <li class="product-item">
-                                                        <a class="product-item-photo" href="#" title="The Name Product">
-                                                            <img class="product-image-photo" src="{{asset('shopAssets/images/media/index1/minicart.jpg')}}" alt="The Name Product">
-                                                        </a>
-                                                        <div class="product-item-details">
-                                                            <strong class="product-item-name">
-                                                                <a href="#">Donec Ac Tempus</a>
-                                                            </strong>
-                                                            <div class="product-item-price">
-                                                                <span class="price">61,19 €</span>
-                                                            </div>
-                                                            <div class="product-item-qty">
-                                                                <span class="label">Qty: </span ><span class="number">1</span>
-                                                            </div>
-                                                            <div class="product-item-actions">
-                                                                <a class="action delete" href="#" title="Remove item">
-                                                                    <span>Remove</span>
+                            @if (Auth::check())
+                                <!-- block mini cart -->
+                                <div class="block-minicart dropdown">
+                                    <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                        <span class="cart-icon"></span>
+                                        <span class="counter qty">
+                                            <span class="cart-text">Shopping Cart</span>
+                                            <span class="counter-number">{{ $cartItems->count() }}</span>
+                                            <span class="counter-label">{{ $cartItems->count() }}
+                                                <span>Items</span></span>
+                                            <span class="counter-price">${{ $total }}</span>
+                                        </span>
+                                    </a>
+                                    <div class="dropdown-menu">
+                                        <form id="checkout_form">
+                                            <div class="minicart-content-wrapper">
+                                                <div class="subtitle">
+                                                    You have {{ $cartItems->count() }} item(s) in your cart
+                                                </div>
+                                                <div class="minicart-items-wrapper">
+                                                    <ol class="minicart-items">
+                                                        @foreach ($cartItems as $cartItem)
+                                                            <li class="product-item">
+                                                                <a class="product-item-photo" href="#"
+                                                                    title="{{ $cartItem->product->title }}">
+                                                                    <img class="product-image-photo"
+                                                                        src="{{ asset($cartItem->product->image) }}"
+                                                                        alt="{{ $cartItem->product->title }}">
                                                                 </a>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="product-item">
-                                                        <a class="product-item-photo" href="#" title="The Name Product">
-                                                            <img class="product-image-photo" src="{{asset('shopAssets/images/media/index1/minicart2.jpg')}}" alt="The Name Product">
-                                                        </a>
-                                                        <div class="product-item-details">
-                                                            <strong class="product-item-name">
-                                                                <a href="#">Donec Ac Tempus</a>
-                                                            </strong>
-                                                            <div class="product-item-price">
-                                                                <span class="price">61,19 €</span>
-                                                            </div>
-                                                            <div class="product-item-qty">
-                                                                <span class="label">Qty: </span ><span class="number">1</span>
-                                                            </div>
-                                                            <div class="product-item-actions">
-                                                                <a class="action delete" href="#" title="Remove item">
-                                                                    <span>Remove</span>
-                                                                </a>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                </ol>
-                                            </div>
-                                            <div class="subtotal">
-                                                <span class="label">Total</span>
-                                                <span class="price">$630</span>
-                                            </div>
-                                            <div class="actions">
-                                                <!-- <a class="btn btn-viewcart" href="">
-                                                    <span>Shopping bag</span>
-                                                </a> -->
-                                                <button class="btn btn-checkout" type="button" title="Check Out">
-                                                    <span>Checkout</span>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div> --}}
-                                <div class="dropdown-menu">
-                                    <form id="checkout_form">
-                                        <div class="minicart-content-wrapper">
-                                            <div class="subtitle">
-                                                You have {{ $cartItems->count() }} item(s) in your cart
-                                            </div>
-                                            <div class="minicart-items-wrapper">
-                                                <ol class="minicart-items">
-                                                    @foreach ($cartItems as $cartItem)
-                                                        <li class="product-item">
-                                                            <a class="product-item-photo" href="#"
-                                                                title="{{ $cartItem->product->title }}">
-                                                                <img class="product-image-photo"
-                                                                    src="{{ asset($cartItem->product->image) }}"
-                                                                    alt="{{ $cartItem->product->title }}">
-                                                            </a>
-                                                            <div class="product-item-details">
-                                                                <strong class="product-item-name">
-                                                                    <a
-                                                                        href="#">{{ $cartItem->product->title }}</a>
-                                                                </strong>
-                                                                <div class="product-item-price">
-                                                                    <span
-                                                                        class="price">{{ $cartItem->product->new_price }}</span>
+                                                                <div class="product-item-details">
+                                                                    <strong class="product-item-name">
+                                                                        <a
+                                                                            href="#">{{ $cartItem->product->title }}</a>
+                                                                    </strong>
+                                                                    <div class="product-item-price">
+                                                                        <span
+                                                                            class="price">{{ $cartItem->product->new_price }}</span>
+                                                                    </div>
+                                                                    <div class="product-item-qty">
+                                                                        <span class="label">Qty: </span><span
+                                                                            class="number">{{ $cartItem->quantity }}</span>
+                                                                    </div>
+                                                                    <div class="product-item-actions">
+                                                                        <a class="action delete" href="#"
+                                                                            title="Remove item">
+                                                                            <span>Remove</span>
+                                                                        </a>
+                                                                    </div>
                                                                 </div>
-                                                                <div class="product-item-qty">
-                                                                    <span class="label">Qty: </span><span
-                                                                        class="number">{{ $cartItem->quantity }}</span>
-                                                                </div>
-                                                                <div class="product-item-actions">
-                                                                    <a class="action delete" href="#"
-                                                                        title="Remove item">
-                                                                        <span>Remove</span>
-                                                                    </a>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                    @endforeach
-                                                </ol>
+                                                            </li>
+                                                        @endforeach
+                                                    </ol>
+                                                </div>
+                                                <div class="subtotal">
+                                                    <span class="label">Total</span>
+                                                    <span class="price">${{ $total }}</span>
+                                                </div>
+                                                <div class="actions">
+                                                    <button class="btn btn-checkout" type="button"
+                                                        title="Check Out">
+                                                        <span>Checkout</span>
+                                                    </button>
+                                                </div>
                                             </div>
-                                            <div class="subtotal">
-                                                <span class="label">Total</span>
-                                                <span class="price">${{ $total }}</span>
-                                            </div>
-                                            <div class="actions">
-                                                <button class="btn btn-checkout" type="button" title="Check Out">
-                                                    <span>Checkout</span>
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
+
                                 <script>
                                     $(document).ready(function() {
                                         $('.btn-checkout').click(function() {
-                                            var productId = '{{ $cartItem->product_id }}';
-                                            var quantity = '{{ $cartItem->quantity }}';
+                                            var productId = $(this).closest('.product-item').find('.product-item-photo').data(
+                                                'product_id');
+                                            var quantity = $(this).closest('.product-item').find('.product-item-qty .number').text();
 
                                             console.log('productId:', productId);
                                             console.log('quantity:', quantity);
@@ -362,7 +301,8 @@
                                         });
                                     });
                                 </script>
-                            </div>
+                            @endif
+
 
                         </div>
 
