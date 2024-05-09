fd7a3a2766f38852a5baf941579a4d31cde22f65
hassasultan <h.hsultan277@gmail.com>
2024-05-09 16:06:26 +0500

    pushed

--- resources/views/ShopFrontend/product-detail.blade.php
+++ resources/views/ShopFrontend/product-detail.blade.php
@@ -1022,7 +1022,7 @@
                 $('#product-listing').html('');
 
                 // Apply skeleton loading structure
-                for (let i = 0; i < 9; i++) { // Assuming 9 products per page
+                for (let i = 0; i < 18; i++) { // Assuming 9 products per page
                     var skeletonHtml = `
                 <li class="col-sm-4 product-item">
                     <div class="skeleton-item">
