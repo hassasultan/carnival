5eeabd4b0b9ff5e9b419507a920cbb8c704c834d
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-05-19 17:47:12 +0500

    s

--- app/Http/Controllers/CarnivalCommitteeController.php
+++ app/Http/Controllers/CarnivalCommitteeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Role;
+use App\Models\Package;
+use App\Models\Vendor;
 
 class CarnivalCommitteeController extends Controller
 {
     public function create($id)
     {
-        return view('carnival_committee.user.index');
+        $roles = Role::where('status', 1)->get();
+        $packages = Package::where('status', 1)->get();
+        $vendors = Vendor::with('user')->where('status', 1)->get();
+        return view('carnival_committee.user.index', compact('roles', 'packages', 'vendors'));
     }
 }
