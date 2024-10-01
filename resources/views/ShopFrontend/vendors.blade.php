7aecb67621dff1f15bc4807cc30aea43ad78c7ec
hassasultan <96184043+hassasultan@users.noreply.github.com>
2024-10-01 19:34:44 +0500

    pushed

--- resources/views/ShopFrontend/vendors.blade.php
+++ resources/views/ShopFrontend/vendors.blade.php
@@ -200,34 +200,35 @@
                     <div class="block-sidebar block-banner-sidebar">
                         <div class="owl-carousel owl-loaded owl-drag" data-nav="false" data-dots="true" data-margin="0"
                             data-items="1" data-autoplaytimeout="700" data-autoplay="true" data-loop="true">
-                    
+
                             <div class="owl-stage-outer">
-                                <div class="owl-stage">
-                                    @foreach($adv1 as $key => $row)
+                                @foreach ($adv1 as $key => $row)
+                                    <div class="owl-stage">
                                         <div class="owl-item">
                                             <div class="item item{{ $key }}">
-                                                <img src="{{ asset('images/' . $row->image) }}" alt="{{ $banner->alt_text }}">
+                                                <img src="{{ asset('images/' . $row->image) }}"
+                                                    alt="{{ $banner->alt_text }}">
                                             </div>
                                         </div>
-                                    @endforeach
-                                </div>
+                                    </div>
+                                @endforeach
                             </div>
-                    
+
                             <div class="owl-nav disabled">
                                 <div class="owl-prev"></div>
                                 <div class="owl-next"></div>
                             </div>
-                    
+
                             <div class="owl-dots">
-                                @foreach($adv1 as $key => $row)
-                                    <div class="owl-dot @if($key == 0) active @endif"><span></span></div>
+                                @foreach ($adv1 as $key => $row)
+                                    <div class="owl-dot @if ($key == 0) active @endif"><span></span></div>
                                 @endforeach
                             </div>
                         </div>
                     </div>
-                    
 
-                    
+
+
                     <!-- block slide top -->
                     <div class="block-sidebar block-banner-sidebar">
                         <div class="owl-carousel owl-loaded owl-drag" data-nav="false" data-dots="true" data-margin="0"
@@ -239,11 +240,11 @@
                                 <div class="owl-stage"
                                     style="transform: translate3d(-844px, 0px, 0px); transition: all 0.8s ease 0s; width: 1477px;">
                                     @foreach ($adv2 as $key => $row)
-                                    <div class="owl-item cloned" style="width: 211px;">
-                                        <div class="item item{{$key}}">
-                                            <img src="/images/{{ $row->image }}" alt="images">
+                                        <div class="owl-item cloned" style="width: 211px;">
+                                            <div class="item item{{ $key }}">
+                                                <img src="/images/{{ $row->image }}" alt="images">
+                                            </div>
                                         </div>
-                                    </div>
                                     @endforeach
                                     {{-- <div class="owl-item cloned" style="width: 211px;">
                                         <div class="item item3">
@@ -413,18 +414,18 @@
 @section('script')
     <script>
         $(document).ready(function() {
-            
+
             var getRegion = '';
-            
+
             $(document).on('click', '.getWithRegion', function(e) {
                 var region = $(this).find($('.region_name')).val();
-                
+
                 getRegion = region
                 console.log('regionregion', getRegion);
-                
+
                 fetchProducts();
             });
-            
+
             console.log('regionregion22', getRegion);
 
             function fetchProducts(page = 1) {
@@ -580,7 +581,6 @@
     </script>
 
     <script>
-        $(document).ready(function() {
-        });
+        $(document).ready(function() {});
     </script>
 @endsection
