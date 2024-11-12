62d1930eb34e184bfc61f65b177086291b76b68d
hassasultan <96184043+hassasultan@users.noreply.github.com>
2024-11-12 17:12:26 +0500

    pushed

--- resources/views/ShopFrontend/vendorEvents.blade.php
+++ resources/views/ShopFrontend/vendorEvents.blade.php
@@ -622,6 +622,45 @@
 @section('script')
     <!-- Custom scripts -->
     <script>
+
+        (function($) {
+
+            "use strict";
+
+            $(document).ready(function() {
+
+                /*  [ Filter by price ]
+
+                - - - - - - - - - - - - - - - - - - - - */
+
+                $('#slider-range').slider({
+
+                    range: true,
+
+                    min: 0,
+
+                    max: 500,
+
+                    values: [0, 300],
+
+                    slide: function (event, ui) {
+
+                        $('#amount-left').text(ui.values[0] );
+                        $('#amount-right').text(ui.values[1] );
+
+                    }
+
+                });
+
+                $('#amount-left').text($('#slider-range').slider('values', 0));
+
+                $('#amount-right').text($('#slider-range').slider('values', 1));
+            });
+
+        })(jQuery);
+
+    </script>
+    <script>
         $(document).ready(function() {
             $('body').addClass('catalog-category-view catalog-view_op1');
 
