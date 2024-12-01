1ad1770cbad7f1f06d10fcd7251b0e597db22e39
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-12-02 01:33:54 +0500

    s

--- app/Http/Controllers/SiteGalleryController.php
+++ app/Http/Controllers/SiteGalleryController.php
@@ -21,29 +21,6 @@ class SiteGalleryController extends Controller
         return view('dashboard.admin.site_gallery.create');
     }
 
-    // public function store(Request $request)
-    // {
-    //     $request->validate([
-    //         'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    //         'status' => 'required|integer|in:0,1',
-    //     ]);
-
-    //     if($request->hasfile('images')) {
-    //         foreach($request->file('images') as $image) {
-    //             $imageName = time().rand(1,100).'.'.$image->extension();
-    //             $image->move(public_path('images'), $imageName);
-
-    //             SiteGallery::create([
-    //                 'image' => $imageName,
-    //                 'status' => $request->status,
-    //             ]);
-    //         }
-    //     }
-
-    //     return redirect()->route('site_gallery.index')
-    //                      ->with('success', 'Images added successfully.');
-    // }
-
     public function store(Request $request)
     {
         $request->validate([
@@ -99,29 +76,6 @@ class SiteGalleryController extends Controller
         $siteGallery->load('images');
         return view('dashboard.admin.site_gallery.edit', compact('siteGallery'));
     }
-
-    // public function update(Request $request, SiteGallery $siteGallery)
-    // {
-    //     $request->validate([
-    //         'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    //         'status' => 'required|integer|in:0,1',
-    //     ]);
-
-    //     if ($request->hasFile('image')) {
-    //         $imageName = time() . '.' . $request->image->extension();
-    //         $request->image->move(public_path('images'), $imageName);
-    //         $siteGallery->update([
-    //             'image' => $imageName,
-    //             'status' => $request->status,
-    //         ]);
-    //     } else {
-    //         $siteGallery->update($request->only(['status']));
-    //     }
-
-    //     return redirect()->route('site_gallery.index')
-    //         ->with('success', 'Image updated successfully.');
-    // }
-
 
     public function update(Request $request, GalleryAlbum $siteGallery)
     {
