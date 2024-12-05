f2cb0539007833533034fba35837f9ab768afd83
hassasultan <96184043+hassasultan@users.noreply.github.com>
2024-12-05 16:29:21 +0500

    pushed

--- app/Models/Music.php
+++ app/Models/Music.php
@@ -38,6 +38,10 @@ class Music extends Model
 
     protected $dates = ['deleted_at'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
     public function setImagesAttribute($value)
     {
         if (!empty($value)) {
