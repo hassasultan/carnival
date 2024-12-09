d8b3ea11d5310d1b05f61fc8f088345ee388df24
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-12-09 08:10:38 +0500

    a

--- resources/views/ShopFrontend/vendorMusicGallery.blade.php
+++ resources/views/ShopFrontend/vendorMusicGallery.blade.php
@@ -320,7 +320,7 @@
             <div class="gallery">
                 @if (count($siteGallery) > 0)
                 {{-- {{ dd(count($siteGallery), $siteGallery->toArray(), $siteGallery[0]->imagesRelation->toArray()) }} --}}
-                    @foreach ($siteGallery[0]->imagesRelation as $key => $row)
+                    @foreach ($siteGallery[1]->imagesRelation as $key => $row)
                         <img src="{{ asset('images/' . $row->image) }}" alt="Image {{ $key }}"
                             onclick="openModal({{ $key }})">
                     @endforeach
