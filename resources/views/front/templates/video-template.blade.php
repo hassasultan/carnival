daaf03455106be917729a87d9ca09dca7173be72
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-01-28 15:00:16 +0500

    pushed

---
+++ resources/views/front/templates/video-template.blade.php
@@ -0,0 +1,54 @@
+<div class="section active clearfix">
+    <div class="video-bg">
+        <div class="clip">
+            <div class="bg bg-bg-chrome act" style="background-image:url(https://carnivalguide.co/travel/img/home_6/slide_5.jpg)"></div>
+        </div>
+        <div class="vertical-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-xs-12">
+                        <div class="main-title style-2">
+                            <h4 class="category color-white-light">norway tours</h4>
+                            <h1>romantic holidays in norway</h1>
+                            <h3 class="person_price color-red">from $400 per person</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="vertical-align p-148">
+            <div class="slide-tour">
+                <div class="video-click-full">
+                    <div class="video-title">
+                        <div class="item-block style-2">
+                            <div class="video-open">
+                                <img src="https://carnivalguide.co/travel/img/home_2/play.png" alt="palay"
+                                    data-src="http://www.youtube.com/embed/mzPxo7Y6JyA?autoplay=1&amp;fs=1&amp;loop=1&amp;rel=0&amp;showinfo=0&amp;color=white&amp;wmode=opaque">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="vertical-bottom text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-10 col-md-offset-1">
+                        <div class="main-title">
+                            <div class="main-date underline">july <strong>19th</strong> to july
+                                <strong>26th</strong></div>
+                            <p class="color-white-light">Curabitur nunc erat, consequat in erat ut, congue
+                                bibendum nulla. Suspendisse id pharetra lacus, et hendrerit mi quis leo
+                                elementum. Lorem ipsum dolor sit amet, labore et dolore magna aliqua.</p>
+                            <div class="clearfix"></div>
+                            <a href="#" class="c-button b-60 bg-red hv-red-o delay-2"><span>view
+                                    more</span></a>
+                            <a href="#" class="c-button b-60 bg-tr-1 hv-red delay-2"><span>book
+                                    now</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
