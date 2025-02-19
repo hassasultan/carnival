fa3980562e7359792871d5e51de9ca27dc5d0eb5
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-02-19 22:35:26 +0500

    pushed

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -50,7 +50,8 @@ class BannerController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'banner_image' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048|dimensions:width=870,height=328',
+            'banner_image' => 'required|mimes:jpeg,png,jpg,gif,mp4,avi,mov,wmv|max:51200',
+            // 'banner_image' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048|dimensions:width=870,height=328',
             'description' => 'nullable|string',
             'status' => 'boolean',
         ]);
