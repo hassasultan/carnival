b7150b27aa0f0174eb45bd5a76ad009650df7230
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-02-24 15:16:54 +0500

    pushed

--- resources/views/dashboard/admin/banners/edit.blade.php
+++ resources/views/dashboard/admin/banners/edit.blade.php
@@ -13,7 +13,7 @@
             <img src="{{ asset($banner->banner_image) }}" width="100">
         </div>
 
-            <div class="form-group" id="poster_wrapper" @if ($banner->poster != null) style="display: block;" @endif>
+            <div class="form-group" id="poster_wrapper" @if ($banner->poster != null) style="display: block;" @else style="display:none;" @endif>
                 <label for="banner_image">Poster</label>
                 <input type="file" name="poster" id="poster" class="form-control">
             </div>
