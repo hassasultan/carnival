bd064f93c88ce33a37a6fa7339dc0e6b508181d2
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-03-07 03:38:14 +0500

    s

--- app/Http/Controllers/FrontendConroller.php
+++ app/Http/Controllers/FrontendConroller.php
@@ -505,13 +505,13 @@ class FrontendConroller extends Controller
     {
         $carnivals = Carnival::with('country_tabs', 'images')->find($slug);
         // dd($event->toArray());
-        $latestUpcoming = Carnival::where('id', != $carnivals->id)
+        $latestUpcoming = Carnival::where('id', '!=', $carnivals->id)
         ->where('city_id', $carnivals->city_id)
             ->orderBy('start_date', 'desc')
             ->first();
 
         if (!$latestUpcoming) {
-            $latestUpcoming = Carnival::where('id', != $carnivals->id)
+            $latestUpcoming = Carnival::where('id', '!=', $carnivals->id)
             ->where('country_id', $carnivals->country_id)
                 ->orderBy('start_date', 'desc')
                 ->first();
