f0fc8a76fbbe7f4f9e35cbade16f7903162cd8fb
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-03-12 13:53:21 +0500

    s

--- resources/views/ShopFrontend/product-listing.blade.php
+++ resources/views/ShopFrontend/product-listing.blade.php
@@ -1,4 +1,6 @@
-@extends('ShopFrontend.Layouts.layout')
+product listing?
+
+extends('ShopFrontend.Layouts.layout')
 
 @section('main')
     <!-- MAIN -->
@@ -114,11 +116,11 @@
 
                     <div>
                         <form>
-                            <input type="text" fdprocessedid="3rmjh">
-                            <button fdprocessedid="l2xg94">Search</button>
+                            <input type="text" name="searchVal" fdprocessedid="3rmjh">
+                            <button fdprocessedid="l2xg94" id="getSearchVal">Search</button>
                         </form>
                     </div>
-                    
+
                     <div class="catalog-view_op1">
                         <div class=" toolbar-products toolbar-top">
 
@@ -1728,7 +1730,7 @@
     <script>
         $(document).ready(function() {
             function fetchProducts(page = 1, selectedCategories = [], priceRanges = [],
-                selectedBrands = [], productCondition = [], stockCondition = [], sale = null) {
+                selectedBrands = [], productCondition = [], stockCondition = [], sale = null, getSearchVal = '') {
                 $('#product-listing').html('');
 
                 // Apply skeleton loading structure
@@ -1757,7 +1759,8 @@
                         brands: selectedBrands,
                         product_condition: productCondition,
                         stock_condition: stockCondition,
-                        sale: sale
+                        sale: sale,
+                        getSearchVal: getSearchVal
                     },
                     success: function(response) {
                         console.log(response);
@@ -1916,20 +1919,6 @@
                     productCondition, stockCondition, sale);
             });
 
-            // Product type filter
-            // $(document).on('click', '.product_type', function() {
-            //     let selectedCategories = getSelectedCategories();
-            //     let productType = $(this).is(':checked') ? 1 : null;
-            //     let priceRanges = getSelectedPriceRanges();
-            //     let selectedBrands = getSelectedBrands();
-            //     let productCondition = getSelectedProductCondition();
-            //     let stockCondition = getSelectedStockCondition();
-            //     let sale = getSaleStatus();
-
-            //     fetchProducts(1, selectedCategories, priceRanges, selectedBrands,
-            //         productCondition, stockCondition, sale);
-            // });
-
             // Price filter
             $(document).on('click', '.price-checkbox', function() {
                 let selectedCategories = getSelectedCategories();
@@ -2019,6 +2008,22 @@
                 // Initial fetch
                 fetchProducts();
             }
+
+            $('#getSearchVal').on('click', function(e) {
+                e.preventDefault(); // Prevent form submission
+
+                let getSearchVal = $('input[name="searchVal"]').val(); // Get search input value
+
+                let selectedCategories = getSelectedCategories();
+                let priceRanges = getSelectedPriceRanges();
+                let selectedBrands = getSelectedBrands();
+                let productCondition = getSelectedProductCondition();
+                let stockCondition = getSelectedStockCondition();
+                let sale = getSaleStatus();
+
+                fetchProducts(1, selectedCategories, priceRanges, selectedBrands, productCondition,
+                    stockCondition, sale, getSearchVal);
+            });
 
         });
     </script>
