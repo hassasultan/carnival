56e697b33866fdb583dbad75d08462396336c625
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-03-14 15:15:53 +0500

    s

--- resources/views/ShopFrontend/model/detail.blade.php
+++ resources/views/ShopFrontend/model/detail.blade.php
@@ -431,11 +431,11 @@
                                     <div class="contact-item">
                                         <span class="icon fas fa-globe"></span>
                                         <span class="text">Region:</span>
-                                        <p>{{ $user->vendor->continent->name }}</p>
-                                        <a href="https://www.google.com/maps/search/?api=1&query={{ urlencode(optional($user->vendor)->address ?? optional($user->subvendor)->address) }}"
+                                        <p>{{ optional(optional($user->vendor)->continent)->name ?? optional(optional($user->subvendor)->continent)->name ?? 'N/A' }}</p>
+                                        {{-- <a href="https://www.google.com/maps/search/?api=1&query={{ urlencode(optional($user->vendor)->address ?? optional($user->subvendor)->address) }}"
                                             target="_blank" class="text">
                                             {{ optional($user->vendor)->address ?? (optional($user->subvendor)->address ?? 'N/A') }}
-                                        </a>
+                                        </a> --}}
                                     </div>
                                 </div>
                             </div>
