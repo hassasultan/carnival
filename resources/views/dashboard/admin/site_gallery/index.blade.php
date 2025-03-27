4f90beb68110d0c0b93724c53a7ab9a762d67bb8
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-03-27 22:52:32 +0500

    pushed

--- resources/views/dashboard/admin/site_gallery/index.blade.php
+++ resources/views/dashboard/admin/site_gallery/index.blade.php
@@ -44,11 +44,11 @@
                                     </tr>
                                 @endforeach
                             </table>
-                            <div class="d-felx justify-content-center">
+                            {{-- <div class="d-felx justify-content-center">
 
                                 {{ $siteGalleries->links() }}
 
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
