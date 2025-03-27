bea74bde5b9fa6cc74bfbed63e025a78713e73ab
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-03-27 23:17:22 +0500

    pushed

--- app/Http/Controllers/SiteGalleryController.php
+++ app/Http/Controllers/SiteGalleryController.php
@@ -12,7 +12,7 @@ class SiteGalleryController extends Controller
     public function index()
     {
         // $siteGalleries = GalleryAlbum::orderBy('id','DESC')->paginate(10);
-        $siteGalleries = SiteGallery::get();
+        $siteGalleries = SiteGallery::where('user_id',auth()->user()->id)->get();
         // dd($siteGalleries->toArray());
         return view('dashboard.admin.site_gallery.index', compact('siteGalleries'));
     }
