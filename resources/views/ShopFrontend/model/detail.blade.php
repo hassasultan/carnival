b18642416c17154a7c72e5505a0c06733ee82477
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-04-03 21:23:40 +0500

    s

--- resources/views/ShopFrontend/model/detail.blade.php
+++ resources/views/ShopFrontend/model/detail.blade.php
@@ -549,40 +549,11 @@
                         <!-- product tab info -->
 
 
-                        {{-- <div class="product-info-detailed">
-
-                            @include('shop-frontend.partials.detail-section')
-                        </div> --}}
                         <div class="product-info-detailed">
 
-                            <!-- Nav tabs -->
-                            <ul class="nav nav-pills" role="tablist">
-                                <li role="presentation" class="active"><a href="#description" role="tab"
-                                        data-toggle="tab">About </a></li>
-                                <li role="presentation"><a href="#tags" role="tab" data-toggle="tab">Section
-                                        Details
-                                    </a>
-                                </li>
-                                <li role="presentation"><a href="#reviews" role="tab" data-toggle="tab">reviews</a>
-                                </li>
-                                <li role="presentation"><a href="#additional" role="tab"
-                                        data-toggle="tab">Experience</a></li>
-                                <li role="presentation"><a href="#tab-cust" role="tab" data-toggle="tab">Additional
-                                        Info</a>
-                                </li>
-                            </ul>
-
-                            <!-- Tab panes -->
-                            <div class="tab-content">
-                                <div role="tabpanel" class="tab-pane active" id="description">
-                                    <div class="block-title">Product Details</div>
-                                    <div class="block-content">
-                                        {!! $event->description !!}
-
-                                    </div>
-                                </div>
-                            </div>
+                            @include('shop-frontend.partials.detail-section')
                         </div>
+                        
                         <!-- product tab info -->
 
                         <!-- block-related product -->
--- resources/views/shop-frontend/partials/detail-section.blade.php
+++ resources/views/shop-frontend/partials/detail-section.blade.php
@@ -1,13 +1,17 @@
+<div class="product-info-detailed">
+
     <!-- Nav tabs -->
     <ul class="nav nav-pills" role="tablist">
-        <li role="presentation" class="active"><a href="#description" role="tab" data-toggle="tab">About </a></li>
+        <li role="presentation" class="active"><a href="#description" role="tab"
+                data-toggle="tab">About </a></li>
         <li role="presentation"><a href="#tags" role="tab" data-toggle="tab">Section
                 Details
             </a>
         </li>
         <li role="presentation"><a href="#reviews" role="tab" data-toggle="tab">reviews</a>
         </li>
-        <li role="presentation"><a href="#additional" role="tab" data-toggle="tab">Experience</a></li>
+        <li role="presentation"><a href="#additional" role="tab"
+                data-toggle="tab">Experience</a></li>
         <li role="presentation"><a href="#tab-cust" role="tab" data-toggle="tab">Additional
                 Info</a>
         </li>
@@ -18,8 +22,9 @@
         <div role="tabpanel" class="tab-pane active" id="description">
             <div class="block-title">Product Details</div>
             <div class="block-content">
-                {!! $user->description !!}
+                {!! $event->description !!}
 
             </div>
         </div>
     </div>
+</div>
