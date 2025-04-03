dc4fee0863a6f45691b3088906d1e25df1e2bcd1
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-04-03 18:50:04 +0500

    update social icon

--- resources/views/ShopFrontend/model/detail.blade.php
+++ resources/views/ShopFrontend/model/detail.blade.php
@@ -440,37 +440,37 @@
                                             <a class="social-btn facebook"
                                                 href="{{ optional($user->vendor)->facebook ?? optional($user->subvendor)->facebook }}">
                                                 <span class="icon fab fa-facebook"></span>
-                                                <span class="text">Facebook</span>
+                                                {{-- <span class="text">Facebook</span> --}}
                                             </a>
                                             <a class="social-btn twitter"
                                                 href="{{ optional($user->vendor)->twitter ?? optional($user->subvendor)->twitter }}">
                                                 <span class="icon fab fa-twitter"></span>
-                                                <span class="text">Twitter</span>
+                                                {{-- <span class="text">Twitter</span> --}}
                                             </a>
                                             <a class="social-btn instagram"
                                                 href="{{ optional($user->vendor)->insta ?? optional($user->subvendor)->insta }}">
                                                 <span class="icon fab fa-instagram"></span>
-                                                <span class="text">Instagram</span>
+                                                {{-- <span class="text">Instagram</span> --}}
                                             </a>
                                             <a class="social-btn linkedin"
                                                 href="{{ optional($user->vendor)->linkedin ?? optional($user->subvendor)->linkedin }}">
                                                 <span class="icon fab fa-linkedin"></span>
-                                                <span class="text">LinkedIn</span>
+                                                {{-- <span class="text">LinkedIn</span> --}}
                                             </a>
                                             <a class="social-btn youtube"
                                                 href="{{ optional($user->vendor)->youtube ?? optional($user->subvendor)->youtube }}">
                                                 <span class="icon fab fa-youtube"></span>
-                                                <span class="text">YouTube</span>
+                                                {{-- <span class="text">YouTube</span> --}}
                                             </a>
                                             <a class="social-btn tiktok"
                                                 href="{{ optional($user->vendor)->tiktok ?? optional($user->subvendor)->tiktok }}">
                                                 <span class="icon fab fa-tiktok"></span>
-                                                <span class="text">TikTok</span>
+                                                {{-- <span class="text">TikTok</span> --}}
                                             </a>
                                             <a class="social-btn whatsapp"
                                                 href="{{ optional($user->vendor)->wa_business_page ?? optional($user->subvendor)->wa_business_page }}">
                                                 <span class="icon fab fa-whatsapp"></span>
-                                                <span class="text">WhatsApp</span>
+                                                {{-- <span class="text">WhatsApp</span> --}}
                                             </a>
                                         </div>
                                     </div>
