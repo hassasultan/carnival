9176a0112ed2e860b4851d6369f4642d7abd5622
alinaqvi1995 <alinaqvif@gmail.com>
2025-04-09 10:49:22 +0500

    s

--- app/Http/Controllers/FrontendConroller.php
+++ app/Http/Controllers/FrontendConroller.php
@@ -106,8 +106,8 @@ class FrontendConroller extends Controller
             return [
                 'id' => $carnival->id,
                 'name' => $carnival->name,
-                'image_url' => 'https://carnivalguide.co/travel/img/home/city_1.jpg',
                 'image_url' => asset($carnival->city->airport),
+                // 'image_url' => 'https://carnivalguide.co/travel/img/home/city_1.jpg',
                 // 'image_url' => 'https://carnivalguide.co/travel/img/home/city_1.jpg',
 
             ];
