2a18d6c5116d3e07e3545aba69a5015ec77edae4
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-04-15 16:36:36 +0500

    debug

--- resources/views/dashboard/vendor/layouts/includes/sidebar.blade.php
+++ resources/views/dashboard/vendor/layouts/includes/sidebar.blade.php
@@ -142,7 +142,7 @@
                 </a>
             </li>
         </ul>
-        {{ dd(auth()->user()->getPackageNameAttribute) }}
+        {{ dd(auth()->user()->vendor()->package->toArray()) }}
         @if (auth()->user()->getPackageNameAttribute == 'Artistes')
             <p class="text-muted nav-heading mt-4 mb-2 pl-4">
                 <span>Artiste (Music)</span>
