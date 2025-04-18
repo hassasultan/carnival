e2f50286ff424329a558557ceb1f40debdc23005
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-04-18 21:58:59 +0500

    pushed

---
+++ resources/views/ShopFrontend/components/sponsers.blade.php
@@ -0,0 +1,19 @@
+<!-- block-brand -->
+<div class="block-brand-op6">
+    <div class="container">
+        <div class="owl-carousel" data-nav="true" data-dots="false" data-margin="2" data-loop="true"
+            data-responsive='{
+            "0":{"items":3},
+            "480":{"items":4},
+            "600":{"items":5},
+            "992":{"items":7}
+        }'>
+            @foreach ($$user->sponsors as $row)
+                <div class="item">
+                    <a href=""><img src="{{ asset('sponser_images/' . $row->logo) }}" alt="brand"></a>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+</div><!-- block-brand -->
--- resources/views/ShopFrontend/model/detail.blade.php
+++ resources/views/ShopFrontend/model/detail.blade.php
@@ -673,6 +673,9 @@
 
                 </div>
             </div>
+            {{-- Sponsers --}}
+            @include('shop-frontend.components.sponsers')
+            {{-- Sponsers --}}
 
             <!-- block  showcase-->
             @include('partials.brand_showcase')
