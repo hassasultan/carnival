aa4488be48c512b92cd9b7a37bdc86ccf2862803
hassasultan <h.hsultan277@gmail.com>
2025-05-13 00:37:03 +0500

    pushed

--- resources/views/ShopFrontend/vendor-detail.blade.php
+++ resources/views/ShopFrontend/vendor-detail.blade.php
@@ -803,7 +803,7 @@
                                                 <div class="product-item  product-item-opt-1 ">
                                                     <div class="product-item-info">
                                                         <div class="product-item-photo">
-                                                            <a class="product-item-img" href=""><img
+                                                            <a class="product-item-img" href="{{ route('get.products.detail',$item->slug) }}"><img
                                                                     alt="product name"
                                                                     src="{{ asset('productImage/' . $item->image) }}"></a>
                                                             <div class="product-item-actions">
