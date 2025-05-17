93dfe085544ce8e5c27984d90ef80fa97eca498b
alinaqvi1995 <alinaqvif@gmail.com>
2025-05-18 00:48:54 +0500

    s

--- resources/views/dashboard/admin/faqPages/edit.blade.php
+++ resources/views/dashboard/admin/faqPages/edit.blade.php
@@ -9,48 +9,37 @@
                     <strong class="card-title">FAQ Information</strong>
                 </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('faqs.update', $faq->id) }}">
+                    <form method="POST" action="{{ route('faqs_page.update') }}">
                         @csrf
-                        @method('PUT')
                         <div class="row">
                             <div class="col-md-6">
-                                <div class="form-group mb-3 d-none">
-                                    <label for="model">Model</label>
-                                    <select id="model" name="modelable_type" class="form-control" required>
-                                        <option value="" selected disabled>Select Model</option>
-                                        <option value="EventsCountryTab" @if($faq->modelable_type) selected @endif>EventsCountryTab</option>
-                                        <option value="Mascamp" @if($faq->modelable_type) selected @endif>Mascamp</option>
-                                        <option value="Product" @if($faq->modelable_type) selected @endif>Product</option>
-                                    </select>
-                                </div>
                                 <div class="form-group mb-3">
-                                    <label for="model">Select EventsCountryTab</label>
-                                    <select id="model" name="modelable_id" class="form-control" required>
-                                        <option value="" selected disabled>Select Model</option>
-                                        @foreach ($modelVal as $item)
-                                            <option value="{{ $item->id }}"
-                                                @if ($item->id == $faq->modelable_id) selected @endif>{{ $item->tab }}
-                                            </option>
-                                        @endforeach
+                                    <label for="page">Page</label>
+                                    <select id="page" name="page" class="form-control" required>
+                                        <option value="" selected disabled>Select Page</option>
+                                        <option value="AboutUs" @if($faq->page == 'AboutUs') selected @endif>About Us</option>
+                                        <option value="ContactUs" @if($faq->page == 'ContactUs') selected @endif>Contact Us</option>
+                                        {{-- <option value="Mascamp" selected>Mascamp</option>
+                                        <option value="Product" selected>Product</option> --}}
                                     </select>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="question">Question</label>
-                                    <input type="text" id="question" name="question" class="form-control"
-                                        placeholder="Enter FAQ question" value="{{ $faq->question }}" required>
+                                    <input type="text" id="question" name="question" value="{{ $faq->question }}" class="form-control"
+                                        placeholder="Enter FAQ question" required>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="answer">Answer</label>
-                                    <textarea id="answer" name="answer" class="form-control" rows="4" placeholder="Enter FAQ answer" required>{{ $faq->answer }}</textarea>
+                                    <textarea id="answer" name="answer" class="form-control" rows="4" value="{{ $faq->answer }}" placeholder="Enter FAQ answer" required></textarea>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="status">Status</label>
                                     <select id="status" name="status" class="form-control" required>
-                                        <option value="1" @if ($faq->status == 1) selected @endif>Active</option>
-                                        <option value="0" @if ($faq->status == 0) selected @endif>Inactive</option>
+                                        <option value="1" @if($faq->status == '1') selected @endif>Active</option>
+                                        <option value="0" @if($faq->status == '0') selected @endif>Inactive</option>
                                     </select>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Create FAQ</button>
+                                <button type="submit" class="btn btn-primary">Update FAQ</button>
                             </div>
                         </div>
                     </form>
