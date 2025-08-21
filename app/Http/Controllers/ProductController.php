19a672227a79492bbb40718b0b7c8d11a48d1fc2
alinaqvi1995 <alinaqvif@gmail.com>
2025-08-21 12:55:04 +0500

    s

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -67,7 +67,6 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
         $request->validate([
             'title' => 'required',
             'category_id' => 'required',
