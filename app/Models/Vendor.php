5e2c06564e2e64ae1f18e2b6f2276836e816fb1d
alinaqvi1995 <alinaqvif@gmail.com>
2025-08-29 11:40:42 +0500

    s

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -1,84 +1,84 @@
-<?php
+    <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Vendor extends Model
-{
-    use HasFactory, SoftDeletes;
-
-    protected $fillable = [
-        'user_id',
-        'package_id',
-        'status',
-        'name',
-        'email',
-        'phone',
-        'logo',
-        'address',
-        'insta',
-        'facebook',
-        'youtube',
-        'twitter',
-        'tiktok',
-        'wa_business_page',
-        'linkedin',
-        'continent',
-        'short_description',
-        'ecommerce',
-        'events',
-        'music',
-        'appointment',
-        'ad_space',
-        'blogging',
-    ];
-
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'deleted_at',
-    ];
-
-    public function user()
+    class Vendor extends Model
     {
-        return $this->belongsTo(User::class);
-    }
+        use HasFactory, SoftDeletes;
 
-    public function package()
-    {
-        return $this->belongsTo(Package::class);
-    }
+        protected $fillable = [
+            'user_id',
+            'package_id',
+            'status',
+            'name',
+            'email',
+            'phone',
+            'logo',
+            'address',
+            'insta',
+            'facebook',
+            'youtube',
+            'twitter',
+            'tiktok',
+            'wa_business_page',
+            'linkedin',
+            'continent',
+            'short_description',
+            'ecommerce',
+            'events',
+            'music',
+            'appointment',
+            'ad_space',
+            'blogging',
+        ];
 
-    public function subvendor()
-    {
-        return $this->hasMany(SubVendor::class);
-    }
+        protected $dates = [
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        ];
 
-    public function products()
-    {
-        return $this->hasMany(Product::class, 'user_id', 'user_id');
-    }
+        public function user()
+        {
+            return $this->belongsTo(User::class);
+        }
 
-    public function continent()
-    {
-        return $this->belongsTo(Region::class, 'continent');
-    }
-    public function region()
-    {
-        return $this->belongsTo(Region::class, 'continent');
-    }
+        public function package()
+        {
+            return $this->belongsTo(Package::class);
+        }
 
-    public function gallery()
-    {
-        return $this->hasMany(SiteGallery::class, 'user_id', 'user_id');
-    }
+        public function subvendor()
+        {
+            return $this->hasMany(SubVendor::class);
+        }
 
-    public function carnivals()
-    {
-        return $this->belongsToMany(Carnival::class, 'pivot_carnival_mascamps', 'mascamp_id', 'carnival_id')
-                    ->withTimestamps();
+        public function products()
+        {
+            return $this->hasMany(Product::class, 'user_id', 'user_id');
+        }
+
+        public function continent()
+        {
+            return $this->belongsTo(Region::class, 'continent');
+        }
+        public function region()
+        {
+            return $this->belongsTo(Region::class, 'continent');
+        }
+
+        public function gallery()
+        {
+            return $this->hasMany(SiteGallery::class, 'user_id', 'user_id');
+        }
+
+        public function carnivals()
+        {
+            return $this->belongsToMany(Carnival::class, 'pivot_carnival_mascamps', 'mascamp_id', 'carnival_id')
+                        ->withTimestamps();
+        }
     }
-}
