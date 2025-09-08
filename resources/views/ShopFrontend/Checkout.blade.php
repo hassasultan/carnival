e76cfa66754b14eb54e541fb805bf3cf17f5ead0
alinaqvi1995 <alinaqvif@gmail.com>
2025-09-08 11:07:19 +0500

    s

--- resources/views/ShopFrontend/Checkout.blade.php
+++ resources/views/ShopFrontend/Checkout.blade.php
@@ -529,6 +529,8 @@
 @endsection
 
 @section('script')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <!-- Custom scripts -->
     <script>
         $(document).ready(function() {
