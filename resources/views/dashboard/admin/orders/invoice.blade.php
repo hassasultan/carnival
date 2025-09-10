307fc388a27b8b7bf2a2da2bcde66fcb9442bcef
alinaqvi1995 <alinaqvif@gmail.com>
2025-09-10 15:24:23 +0500

    s

--- resources/views/dashboard/admin/orders/invoice.blade.php
+++ resources/views/dashboard/admin/orders/invoice.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <title>Invoice #{{ $order->order_num }}</title>
     <link rel="icon" type="image/png" href="{{ asset('shopAssets/images/logo.png') }}">
+    <!-- Favicon -->
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
